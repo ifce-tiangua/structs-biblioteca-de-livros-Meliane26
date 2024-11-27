1b1949a456bc0e3f99ce100d3affb3aedda0b6f8
Meliane26 <maria.meliane17@gmail.com>
2024-11-26 23:22:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -16,19 +16,34 @@ pLivro livro_aloc(int qtde) {
     return (pLivro)malloc(qtde * sizeof(Livro));
 }
 
+void limpa_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 void livro_ler(pLivro livros, int qtde) {
     for (int i = 0; i < qtde; i++) {
         printf("=== Livro %d ===\n", i + 1);
+
         printf("Nome: ");
-        scanf(" %[^\n]", livros[i].nome);
+        limpa_buffer();
+        fgets(livros[i].nome, sizeof(livros[i].nome), stdin);
+        livros[i].nome[strcspn(livros[i].nome, "\n")] = '\0';
+
         printf("ISBN: ");
-        scanf(" %[^\n]", livros[i].isbn);
+        fgets(livros[i].isbn, sizeof(livros[i].isbn), stdin);
+        livros[i].isbn[strcspn(livros[i].isbn, "\n")] = '\0';
+
         printf("Preço: ");
         scanf("%f", &livros[i].preco);
+
         printf("Score (0-10): ");
         scanf("%d", &livros[i].score);
+
         printf("Editora: ");
-        scanf(" %[^\n]", livros[i].editora);
+        limpa_buffer();
+        fgets(livros[i].editora, sizeof(livros[i].editora), stdin);
+        livros[i].editora[strcspn(livros[i].editora, "\n")] = '\0';
     }
 }
 
